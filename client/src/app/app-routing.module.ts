d534cd739567e44e07f2ceb28d9fb2105cfd2be9
Andrii Kapran <kapranandriyu@gmail.com>
2023-07-14 10:35:06 +0200

    added login page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from 'src/app/components/home/home.component';
+import { LoginComponent } from 'src/app/components/auth/login/login.component';
+import { SignUpComponent } from 'src/app/components/auth/sign-up/sign-up.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'account/login', component: LoginComponent },
+  { path: 'account/sign-up', component: SignUpComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,51 @@
+/* Default imports */
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+/* Another import  */
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+/* Components */
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from 'src/app/components/header/header.component';
+import { LoginComponent } from 'src/app/components/auth/login/login.component';
+import { SignUpComponent } from 'src/app/components/auth/sign-up/sign-up.component';
+import { HomeComponent } from 'src/app/components/home/home.component';
+
+/* Angular Material imports */
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    SignUpComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    ReactiveFormsModule,
+    HttpClientModule,
+
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
