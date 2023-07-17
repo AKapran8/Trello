d1a44da59a5429e1011fcd52bbaaefb5085702d3
Ivan Khomenko <simkav2411@gmail.com>
2023-07-17 23:00:51 +0300

    setup corse

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const session = require('express-session')
+const cors = require('cors')
 const { apiRouter } = require('./router')
 const server = express()
 
@@ -10,12 +11,15 @@ server.use(session({
     cookie: { sameSite: 'strict', maxAge: 1000 * 60 * 24 * 7 }
 }))
 
+server.use(cors({ origin: `http://localhost:${process.env.CLIENT_PORT}`, credentials: true }))
+
 server.use(express.json())
 
 
 server.use("/api", apiRouter)
 
 server.use((err, req, res, next) => {
+    console.error('LAST HANDLER ERROR')
     res.statusCode = 400
     res.send(err.message)
 })
